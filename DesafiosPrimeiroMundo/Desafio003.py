cae7e2382fc05cdd3dfe8ed1bc31726bf6ea3e65
josue silva <silvajosue.contato@gmail.com>
2024-01-14 19:08:05 -0300

    commit

--- DesafiosPrimeiroMundo/Desafio003.py
+++ DesafiosPrimeiroMundo/Desafio003.py
@@ -1,8 +1,5 @@
 '''Crie um programa que leia dois números e mostre a soma entre eles.'''
-def new_func():
-    n1 = int(input('Digite um número: '))
-    n2 = int(input('Digite um número: '))
-    s = n1 + n2
-    print('A soma entre eles é:', s)
-
-new_func()
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+s = n1 + n2
+print('A soma entre eles é:',s)
--- DesafiosPrimeiroMundo/Desafio010.py
+++ DesafiosPrimeiroMundo/Desafio010.py
@@ -1,5 +1,5 @@
 '''Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.'''
-#Considere o dolar US$1.00 = R$3,27
+# Considere o dolar US$1.00 = R$3,27
 real = float(input('Quanto dinheiro você tem na carteira? R$'))
 dolar = real / 3.27
 print(f'Com R${real:.2f} você pode comprar US${dolar:.2f} ')
