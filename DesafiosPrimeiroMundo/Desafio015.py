c3000fafb1b9d1fd329dabfbd47adaa5ade2ed60
josue silva <silvajosue.contato@gmail.com>
2024-01-16 15:24:36 -0300

    Commit

---
+++ DesafiosPrimeiroMundo/Desafio015.py
@@ -0,0 +1,3 @@
+'''Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.'''
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {int(num)}')
